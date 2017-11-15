45eee43d7d1459c023f92d682a1e93feec96e5ca
sarmsaad <sarah.msaad@yahoo.com>
2017-11-15 11:37:22 -0500

    gets entities and saves them to a list

--- src/main/java/org/archivemanager/portal/web/DigitalReserveController.java
+++ src/main/java/org/archivemanager/portal/web/DigitalReserveController.java
@@ -1,7 +1,12 @@
 package org.archivemanager.portal.web;
 
+import java.util.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.heed.openapps.entity.Entity;
+import org.heed.openapps.search.SearchResult;
 
 import org.heed.openapps.data.RestResponse;
 import org.heed.openapps.entity.EntityService;
@@ -21,18 +26,56 @@ import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
 public class DigitalReserveController {
 	private EntityService entityService;
 	private SearchService searchService;
-	
-	
+
+	List<Entity> list = new ArrayList<Entity>();
+	List<Long> list_id = new ArrayList<Long>();
+
 	@ResponseBody
 	@RequestMapping(value="/get.json", method = RequestMethod.GET)
-	public RestResponse<Object> fetchEntity(HttpServletRequest request, HttpServletResponse response,
+	public String getEntity(HttpServletRequest request, HttpServletResponse response,
 			@RequestParam(required=false) Long id) throws Exception {
-		RestResponse<Object> data = new RestResponse<Object>();
-		
-	
-		
-		
-		return data;
+				//add an the entity if provided an id
+				if(id != null){
+					if(!list_id.contains(id)){
+						entityService = getEntityService();
+						Entity ent = entityService.getEntity(id);
+						list.add(ent);
+						list_id.add(id);
+					}
+				}
+				//return the current list of entities' uid's after iterating through them
+				int len = list.size();
+				String result = "";
+				for(int i = 0; i<len; i++){
+					Entity ent = list.get(i);
+					SearchResult something = new SearchResult(ent);
+					result = result + "\n" + something.getUid();
+				}
+				return result;
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/add.json", method = RequestMethod.GET)
+	public void addEntity(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(required=true) Long id) throws Exception {
+				//add the entity to the list
+			if(!list_id.contains(id)){
+		    entityService = getEntityService();
+		    Entity ent = entityService.getEntity(id);
+				list.add(ent);
+				list_id.add(id);
+			}
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/delete.json", method = RequestMethod.GET)
+	public String deleteEntity(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(required=true) Long id) throws Exception {
+				//delete the entity to the list
+        entityService = getEntityService();
+        Entity ent = entityService.getEntity(id);
+				boolean yon = list.remove(ent);
+				return "DELETED: " + String.valueOf(yon);
 	}
 	
 	
